2e066cb7fc3a85914dfb54da6d9c1d9c51c4d00d
arun ekambaram <arunekambaram173@gmail.com>
2023-01-27 21:47:46 +0530

    Updated Version

--- Patterns/Patterns.java
+++ Patterns/Patterns.java
@@ -177,8 +177,16 @@ public class Patterns{
 
     public static void pattern12(int n){
         for(int i=0;i<n;i++){
+            int start =1;
+            if(i%2==0){
+                start =1;
+            }
+            else{
+                start =0;
+            }
             for(int j=0;j<=i;j++){
-                System.out.print("1 ");
+                System.out.print(start+" ");
+                start=1-start;
             }
             System.out.println("");
         }
