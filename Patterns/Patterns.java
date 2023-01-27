b34d869d67cbe4bd964b01d29ae5736904212c5a
arun ekambaram <arunekambaram173@gmail.com>
2023-01-27 21:19:50 +0530

    Updated version

--- Patterns/Patterns.java
+++ Patterns/Patterns.java
@@ -83,14 +83,115 @@ public class Patterns{
         }
     }
 
+    public static void pattern8(int n){
+        for(int i=0;i<n;i++){
+            for(int j=0;j<=i;j++){
+                System.out.print(" ");
+            }
 
+            for(int j=0;j<(2*n-1-2*i);j++){
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+      
+    }
+    public static void pattern9(int n){
+        for(int i=0;i<n;i++){
+            //space
+            for(int j=0; j<n-i-1;j++){
+                System.out.print(" ");
+            }
+            //star
+            for(int j=0; j<2*i+1;j++){
+                System.out.print("*");
+            }
+            //space
+            // for(int j=0; j<=n-i-1;j++){
+            //     System.out.print(" ");
 
+            // }
+            System.out.println("");
+        }
+
+        for(int i=0;i<n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(" ");
+            }
+
+            for(int j=0;j<(2*n-1-2*i);j++){
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+
+    }
+// for even numbers
+    void pattern10(int n) {
+        for(int i=0;i<n;i++){
+           //space
+           for(int j=0; j<n-i-1;j++){
+               System.out.print(" ");
+           }
+           //star
+           for(int j=0; j<i+1;j++){
+               System.out.print("* ");
+           }
+           //space
+           // for(int j=0; j<=n-i-1;j++){
+           //     System.out.print(" ");
+
+           // }
+           System.out.println("");
+       }
+
+       for(int i=0;i<n;i++){
+           for(int j=1;j<=i;j++){
+               System.out.print(" ");
+           }
+
+           for(int j=0;j<(n-i);j++){
+               System.out.print("* ");
+           }
+           System.out.println("");
+       }
+       
+   }
+   
+    public static void pattern11(int n){
+        for(int i=0;i<n;i++){
+
+            for(int j=1;j<=i+1;j++){
+                System.out.print("* ");
+        }
+        System.out.println(" ");
+    }
+    for(int i=0;i<n;i++){
+
+        for(int j=1;j<=n-i-1;j++){
+            System.out.print("* ");
+    }
+    System.out.println(" ");
+}
+    }
+
+    public static void pattern12(int n){
+        for(int i=0;i<n;i++){
+            for(int j=0;j<=i;j++){
+                System.out.print("1 ");
+            }
+            System.out.println("");
+        }
+     
+    }
+
+   
 
 
 
     public static void main(String[] args){
         Patterns pattern = new Patterns();
-        pattern.pattern7(5);
+        pattern.pattern12(5);
 
     }
 }
