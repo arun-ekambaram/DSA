14dceb4f60ff442ec1aa0ea871fed9462969edf7
arun ekambaram <arunekambaram173@gmail.com>
2023-03-06 00:52:53 +0530

    first

--- hashing/hashing.java
+++ hashing/hashing.java
@@ -1,13 +1,20 @@
 package hashing;
 
 public class hashing {
+
    
     public static void frequencyCount(int arr[], int N, int P)
     {
-        N = arr[N];
-        for(int i=0; i<n;i++){
-            arr[i];
+
+        
+        int hash[] = {0};
+
+        for(int i=0; i < arr.length;i++){
+
+                hash[arr[i]] += 1;
         }
+
+        
 
        
 
